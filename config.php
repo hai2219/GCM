7fa9f350b6c6444c9f71297010002dbb789ca124
Hai Ngo <hai.ngothanh@rainmaker-labs.com>
2016-03-21 09:27:09 +0700

    up source

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Database config variables
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "Rain@1");
+define("DB_DATABASE", "gcm");
+
+// Google Cloud Messaging API Key
+// Place your Google API Key
+define("GOOGLE_API_KEY", "AIzaSyBhWUMHVfWuX4yo6_7Yc79EDQSuguGi0R8");
+
+?>
